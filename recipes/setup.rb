a42757e049ac98279cc8f26a5009d80359300216
root <root@ip-172-31-64-134.ec2.internal>
2017-12-12 06:55:30 +0000

    initial commit for workstation cookbook tracking with VCS

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do 
+  action :install
+end
+
+package 'ntp'
+package 'git' do
+  action :install
+end
+file '/etc/motd' do
+  content 'This server is the property of TechnoTrainer'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
